ebe05cd6337b0ae2ca990fe6fc94230ac2466306
Nahuel <matiasghilardisalinas@gmail.com>
2025-05-30 21:05:17 -0300

    fix errors in app.py and add json in app.py

--- models/registroHeroe.py
+++ models/registroHeroe.py
@@ -1,17 +1,17 @@
 import uuid
-from models.db import db  
+from models.db import db 
 
-class VariedadUva(db.Model):
-    __tablename__ = 'Vengador'  # Nombre explícito para la tabla en la base de datos
+
+class Vengador(db.Model): 
+    __tablename__ = 'Vengador' 
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     nombre = db.Column(db.String(100), nullable=False)
     alias = db.Column(db.String(100), nullable=True)
-    habilidades = db.Column(db.String(100), nullable=True)
+    habilidades = db.Column(db.Text, nullable=True) 
     actor = db.Column(db.String(200), nullable=True)
 
 
     def __repr__(self):
         # Método para mostrar una representación legible del objeto (útil para debug)
-        return f"<vengadores {self.nombre} (id={self.id})>"
-    
+        return f"<Vengador {self.nombre} (id={self.id})>"
